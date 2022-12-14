23356f9323db2ddbd4392d3b61dc576d1d67b358
Rustam <rustam1222@gmail.com>
2022-12-14 20:09:20 +0200

    created sort component

---
+++ src/components/blocks/CatalogSort.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function CatalogSort() {
+    const [open, setOpen] = React.useState(false);
+    const [selected, setSelected] = React.useState(0);
+    const list = ['Price: Low to high','Price: High to low']
+
+    const onClickListItem = (i) => {
+        setSelected(i);
+        setOpen(false);
+      }
+
+    return (
+        <div className="sort">
+                <div className="sort__label">
+                  <p>Sort by:</p>
+                  <span onClick={() => setOpen(!open)}> {list[selected]} </span>
+                  <div onClick={() =>setOpen(!open)} className="sort__icon icon-shadow icon-32px">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M3.33301 3.33301H25.9997V25.9997H3.33301V3.33301Z" fill="#4EFF9F" />
+                      <path fillRule="evenodd" clipRule="evenodd" d="M2.66602 3.33268C2.66602 2.96449 2.96449 2.66602 3.33268 2.66602H25.9993C26.3675 2.66602 26.666 2.96449 26.666 3.33268V25.9993C26.666 26.3675 26.3675 26.666 25.9993 26.666H3.33268C2.96449 26.666 2.66602 26.3675 2.66602 25.9993V3.33268ZM3.99935 3.99935V25.3327H25.3327V3.99935H3.99935Z" fill="black" />
+                      <path d="M10.3327 21.3333V16H7.66602V21.3333H10.3327Z" fill="black" />
+                      <path fillRule="evenodd" clipRule="evenodd" d="M6.99902 15.333H10.999V21.9997H6.99902V15.333ZM8.33236 16.6663V20.6663H9.66569V16.6663H8.33236Z" fill="black" />
+                      <path d="M15.6657 21.333V13.333H12.999V21.333H15.6657Z" fill="black" />
+                      <path fillRule="evenodd" clipRule="evenodd" d="M12.332 12.666H16.332V21.9993H12.332V12.666ZM13.6654 13.9993V20.666H14.9987V13.9993H13.6654Z" fill="black" />
+                      <path d="M21.332 21.3333V8H18.332V21.3333H21.332Z" fill="black" />
+                      <path fillRule="evenodd" clipRule="evenodd" d="M17.666 7.33301H21.9993V21.9997H17.666V7.33301ZM18.9993 8.66634V20.6663H20.666V8.66634H18.9993Z" fill="black" />
+                    </svg>
+                  </div>
+                </div>
+                {open &&
+                (<div className="sort__popup">
+                  <ul>
+                    {
+                        list.map((name, i) => (
+                            <li key={name} 
+                            onClick={() => onClickListItem(i)}
+                            className={selected === i ? "active" : ""}>
+                                {name}
+                            </li>
+                        ))
+                    }
+                  </ul>
+                </div>)}
+              </div>
+    );
+}
+
+export default CatalogSort;
